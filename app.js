d0d2ef21644b1efac0652331536a7f4d4b5ea8a4
lea moesch <leamoesch@gmail.com>
2022-07-06 14:04:03 +1000

    about

---
+++ app.js
@@ -0,0 +1,48 @@
+// < =============== NAVBAR =============== >
+
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  // Toggle Nav ===>
+  burger.addEventListener('click',() =>{
+    nav.classList.toggle('nav-active');
+
+    // Animate links ===>
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+      }
+    });
+
+      // Burger Animation ===>
+      burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
+
+// < =============== ABOUT =============== >
+
+
+
+    // < =============== RESUME =============== >
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
